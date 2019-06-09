2b2b5ffd2daed644752605610ff752c12557c304
Rajeev Ranjan <r.cse164@gmail.com>
2019-06-09 18:31:59 +0530

    Completed HotelBookingTest and realated located and mthods

--- src/main/java/com/qa/base/Base.java
+++ src/main/java/com/qa/base/Base.java
@@ -3,23 +3,70 @@ package com.qa.base;
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.events.EventFiringWebDriver;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 public class Base {
 
     public static WebDriver driver;
 
-    protected void setDriverPath() {
+
+    public static Properties prop;
+    public  static EventFiringWebDriver e_driver;
+
+    public Base(){
+        try {
+            prop = new Properties();
+            FileInputStream ip = new FileInputStream(System.getProperty("user.dir")+ "/src/main/java/com/qa/config" +
+                    "/config.properties");
+            prop.load(ip);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    public static void initialization(){
+        String browserName = prop.getProperty("browser");
+
+        if(browserName.equals("chrome")){
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+            driver = new ChromeDriver();
+        }
+        else if(browserName.equals("FF")){
+            System.setProperty("webdriver.gecko.driver", "geckodriver");
+            driver = new FirefoxDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+        driver.get(prop.getProperty("url"));
+
+    }
+
+    /*public static void setDriverPath() {
         if (PlatformUtil.isMac()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver");
             driver = new ChromeDriver();
+            driver.get("https://www.cleartrip.com/");
         }
         if (PlatformUtil.isWindows()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
             driver = new ChromeDriver();
+            driver.get("https://www.cleartrip.com/");
         }
         if (PlatformUtil.isLinux()) {
             System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
             driver = new ChromeDriver();
+            driver.get("https://www.cleartrip.com/");
         }
-    }
+    }*/
+
+
 }
--- src/main/java/com/qa/test/SignInTest.java
+++ src/main/java/com/qa/test/SignInTest.java
@@ -1,5 +1,6 @@
 package com.qa.test;
 
+import com.qa.base.Base;
 import com.sun.javafx.PlatformUtil;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -7,16 +8,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class SignInTest {
+import static com.qa.util.Utility.waitFor;
 
-    WebDriver driver = new ChromeDriver();
+public class SignInTest  {
+
+    public WebDriver driver;
 
     @Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
-        setDriverPath();
-
-        driver.get("https://www.cleartrip.com/");
+        //setDriverPath();
+        //driver.get("https://www.cleartrip.com/");
         waitFor(2000);
 
         driver.findElement(By.linkText("Your trips")).click();
@@ -28,26 +30,5 @@ public class SignInTest {
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
     }
-
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
-
 
 }
