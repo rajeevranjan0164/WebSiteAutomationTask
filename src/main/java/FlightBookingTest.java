ab544f18b0e7c48185d0b25369500d98eb893d94
Rajeev Ranjan <r.cse164@gmail.com>
2019-06-09 11:22:06 +0530

    Added one private void setDriverPath() {package

--- src/main/java/FlightBookingTest.java
+++ src/main/java/FlightBookingTest.java
@@ -74,16 +74,4 @@ public class FlightBookingTest {
             return false;
         }
     }
-
-    private void setDriverPath() {
-        if (PlatformUtil.isMac()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver");
-        }
-        if (PlatformUtil.isWindows()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
-        }
-        if (PlatformUtil.isLinux()) {
-            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
-        }
-    }
 }
---
+++ src/main/java/com/qa/base/Base.java
@@ -0,0 +1,18 @@
+package com.qa.base;
+
+import com.sun.javafx.PlatformUtil;
+
+public class Base {
+
+    protected void setDriverPath() {
+        if (PlatformUtil.isMac()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver");
+        }
+        if (PlatformUtil.isWindows()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver.exe");
+        }
+        if (PlatformUtil.isLinux()) {
+            System.setProperty("webdriver.chrome.driver", "chromedriver_linux");
+        }
+    }
+}
